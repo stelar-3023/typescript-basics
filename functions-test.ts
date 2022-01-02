fbf7b2a1e95cdf885453c32b8a99702e0bc80c86
Steve Larsen <slarsen-3@att.net>
2022-01-01 20:37:44 -0600

    functions-test.ts

---
+++ functions-test.ts
@@ -0,0 +1,5 @@
+import addNumbers, { addStrings, getName } from './functions';
+
+console.log(addNumbers(1, 2));
+console.log(addStrings('Hello', 'World'));
+console.log(getName({first: 'John', last: 'Doe'}));
